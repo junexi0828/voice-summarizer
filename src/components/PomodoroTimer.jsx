8afd67e0ae6d237a8a1f20a64ce2138519d8a4b6
junexi0828 <junexi0828@gmail.com>
2025-08-06 16:18:02 +0900

    feat: 알림 배너 및 실제 소리 기능 추가 - 포모도로 타이머 완료 시 알림음과 시각적 알림 배너 구현

--- src/components/PomodoroTimer.jsx
+++ src/components/PomodoroTimer.jsx
@@ -418,8 +418,8 @@ const PomodoroTimer = () => {
         {/* 알림음 */}
         <audio ref={audioRef} preload="auto">
           <source
-            src="data:audio/mp3;base64,SUQzBAAAAAAAI1RTU0UAAAAPAAADTGF2ZjU4LjIwLjEwMAAAAAAAAAAAAAAA//tQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWGluZwAAAA8AAAACAAAFbgBtbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1tbW1t//////////////////////////////////////////////////////////////////8AAAAATGF2YzU4LjM1AAAAAAAAAAAAAAAAJAYAAAAAAAAABW4A"
-            type="audio/mp3"
+            src="data:audio/wav;base64,UklGRnoGAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQoGAACBhYqFbF1fdJivrJBhNjVgodDbq2EcBj+a2/LDciUFLIHO8tiJNwgZaLvt559NEAxQp+PwtmMcBjiR1/LMeSwFJHfH8N2QQAoUXrTp66hVFApGn+DyvmwhBSuBzvLZiTYIG2m98OScTgwOUarm7blmGgU7k9n1unEiBC13yO/eizEIHWq+8+OWT"
+            type="audio/wav"
           />
         </audio>
 
